9dc839c32c19bb5234520e01370b77ba3e7c7ac8
kliwinski <kliwinski@deloittece.com>
2024-07-15 17:04:08 +0200

    Added solutions for all tasks from module 2 Lesson 1

--- examples/module2/lesson1/jsdom-example/renderer.test.ts
+++ examples/module2/lesson1/jsdom-example/renderer.test.ts
@@ -25,4 +25,30 @@ describe('User renderer', () => {
     renderItems(container, users);
     expect(Array.from(container.querySelectorAll('li'))).toHaveLength(2);
   });
+
+  test('should render correct user data', () => {
+    const userRole = 'user';
+    localStorage.setItem('userRole', userRole);
+    const filteredUsers = users.filter((user) => user.role === userRole);
+    const container = document.createElement('div');
+
+    renderItems(container, users);
+    container.querySelectorAll('li').forEach((element, index) => {
+      expect(element.textContent).toContain(filteredUsers[index].name);
+      expect(element.textContent).toContain(filteredUsers[index].age);
+    });
+  });
+
+  test('should render correct admin data', () => {
+    const userRole = 'admin';
+    localStorage.setItem('userRole', userRole);
+    const container = document.createElement('div');
+
+    renderItems(container, users);
+    container.querySelectorAll('li').forEach((element, index) => {
+      expect(element.textContent).toContain(
+        users[index].role === 'admin' ? '(Admin)' : ''
+      );
+    });
+  });
 });
